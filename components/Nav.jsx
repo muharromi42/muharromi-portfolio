b7156ac73c3fbe83d50f55d7134b2e3be8a0b786
muharromi42 <muharromi42@gmail.com>
2025-01-18 22:40:56 +0700

    desktop nav

---
+++ components/Nav.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "projects",
+    path: "/projects",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
+const Nav = () => {
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, index) => {
+        return (
+          <Link href={link.path} key={index}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Nav;
